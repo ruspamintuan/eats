5d998d39667a264fe0fd64d195832a65c7f9f33a
Russel <russel.p9.rp@gmail.com>
2024-09-15 00:02:12 +0800

    lead query

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,7 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID, GraphQLList } from "graphql";
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID, GraphQLList, GraphQLEnumType } from "graphql";
 import { openDb } from "./db"; // Import your db connection utility
 import { Rows, ExpectedRows } from "./types";
+import { GET_LEADS, GET_SPECIFIC_LEADS } from "./sqlQueries";
 
 // Define LeadsType
 const LeadsType = new GraphQLObjectType({
@@ -20,11 +21,12 @@ const QueryType = new GraphQLObjectType({
   name: "Query",
   fields: {
     leads: {
+      description: "Retrieve all leads",
       type: new GraphQLList(LeadsType),
       resolve: async () => {
         const db = openDb();
         const rows: Rows[] = await new Promise((resolve, reject) => {
-          db.all("SELECT * FROM leads", (err: Error | undefined, row: Rows[]) => {
+          db.all(GET_LEADS, (err: Error, row: Rows[]) => {
             if (err) {
               reject(err);
             } else {
@@ -34,10 +36,10 @@ const QueryType = new GraphQLObjectType({
         });
 
         if (!rows) {
-          throw new Error("no rows found");
+          return;
         }
         const mappedRows: ExpectedRows[] = rows.map((row) => {
-          const services: string[] = JSON.parse(row.services);
+          const services: string[] = JSON.parse(row.services); // since services is stored in sqlite as JSON string, parse back to array
           return {
             ...row,
             services,
@@ -47,8 +49,57 @@ const QueryType = new GraphQLObjectType({
         return mappedRows;
       },
     },
+    lead: {
+      description: "Retrieve lead data based on input",
+      type: LeadsType,
+      args: {
+        email: {
+          type: GraphQLString,
+        },
+        services: {
+          type: GraphQLString,
+        },
+        name: {
+          type: GraphQLString,
+        },
+        mobile: {
+          type: GraphQLString,
+        },
+        postcode: {
+          type: GraphQLString,
+        },
+      },
+      resolve: async (_, args) => {
+        const db = openDb();
+        const row: Rows = await new Promise((resolve, reject) => {
+          const params: Rows = {
+            name: args.name || "",
+            email: args.email || "",
+            services: args.services || "",
+            mobile: args.mobile || "",
+            postcode: args.postcode || "",
+          };
 
-    // TODO: lead query
+          const { query, queryParams } = GET_SPECIFIC_LEADS(params);
+
+          db.get(query, queryParams, (error: Error, row: Rows) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(row);
+            }
+          });
+        });
+
+        if (!row) {
+          return;
+        }
+        return {
+          ...row,
+          services: JSON.parse(row?.services),
+        };
+      },
+    },
   },
 });
 
---
+++ src/sqlQueries.ts
@@ -0,0 +1,40 @@
+import { Rows } from "./types";
+
+export const GET_LEADS = "SELECT * FROM leads";
+
+export const GET_SPECIFIC_LEADS = (params: Rows) => {
+  let filterCondition = "";
+  const queryParams: string[] = [];
+
+  if (params.email) {
+    filterCondition += filterCondition ? " AND " : " WHERE ";
+    filterCondition += "email = ? ";
+    queryParams.push(params.email);
+  }
+  if (params.name) {
+    filterCondition += filterCondition ? " AND " : " WHERE ";
+    filterCondition += "name = ? ";
+    queryParams.push(params.name);
+  }
+  console.log("Filter", filterCondition);
+  if (params.mobile) {
+    filterCondition += filterCondition ? " AND " : " WHERE ";
+    filterCondition += "mobile = ? ";
+    queryParams.push(params.email);
+  }
+  if (params.postcode) {
+    filterCondition += filterCondition ? " AND " : " WHERE ";
+    filterCondition += "postcode = ? ";
+    queryParams.push(params.postcode);
+  }
+  if (params.services) {
+    filterCondition += filterCondition ? " AND " : " WHERE ";
+    filterCondition += "services LIKE ? ";
+    queryParams.push(`%${params.services}%`);
+  }
+
+  return {
+    query: GET_LEADS + filterCondition, // base query with condition
+    queryParams: queryParams,
+  };
+};
